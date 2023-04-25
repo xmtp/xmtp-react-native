9fd95de3e805eea6a3dfed5bb6985888a9e0edf3
Naomi Plasterer <naomi@xmtp.com>
2023-04-25 12:07:53 -0700

    get it compiling locally

--- android/src/main/java/expo/modules/xmtpreactnativesdk/XMTPModule.kt
+++ android/src/main/java/expo/modules/xmtpreactnativesdk/XMTPModule.kt
@@ -1,47 +1,91 @@
 package expo.modules.xmtpreactnativesdk
 
+import android.util.Base64
+import android.util.Base64.NO_WRAP
+import com.google.protobuf.kotlin.toByteString
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import kotlinx.coroutines.suspendCancellableCoroutine
+import org.xmtp.android.library.Client
+import org.xmtp.android.library.Conversation
+import org.xmtp.android.library.SigningKey
+import org.xmtp.android.library.XMTPException
+import org.xmtp.android.library.messages.PrivateKeyBuilder
+import org.xmtp.android.library.messages.Signature
+import org.xmtp.proto.message.contents.SignatureOuterClass
+import java.util.UUID
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+class ReactNativeSigner(var module: XMTPModule, override var address: String) : SigningKey {
+    private var continuations: MutableMap<String, Continuation<Signature>> = mutableMapOf()
+
+    fun handle(id: String, signature: String) {
+        val continuation = continuations[id] ?: return
+        val signatureData = Base64.decode(signature.toByteArray(), NO_WRAP)
+        if (signatureData == null || signatureData.size != 65) {
+            continuation.resumeWithException(XMTPException("Invalid Signature"))
+            continuations.remove(id)
+            return
+        }
+        val sig = SignatureOuterClass.Signature.newBuilder().also {
+            it.ecdsaCompact = it.ecdsaCompact.toBuilder().also { builder ->
+                builder.bytes = signatureData.take(64).toByteArray().toByteString()
+                builder.recovery = signatureData[64].toInt()
+            }.build()
+        }.build()
+        continuation.resume(sig)
+        continuations.remove(id)
+    }
+
+    override suspend fun sign(data: ByteArray): Signature {
+        val request = SignatureRequest(message = String(data, Charsets.UTF_8))
+        module.sendEvent("sign", mapOf(Pair("id", request.id), Pair("message", request.message)))
+        return suspendCancellableCoroutine { continuation ->
+            continuations[request.id] = continuation
+        }
+    }
+
+    override suspend fun sign(message: String): Signature =
+        sign(message.toByteArray())
+}
+
+data class SignatureRequest(
+    var id: String = UUID.randomUUID().toString(),
+    var message: String,
+)
+
+val Conversation.cacheKey: String
+    get() {
+        return if (conversationId != "") {
+            "${topic}:${conversationId}"
+        } else {
+            topic
+        }
+    }
 
 class XMTPModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('XMTP')` in JavaScript.
-    Name("XMTP")
+    var client: Client? = null
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
+    override fun definition() = ModuleDefinition {
+        Name("XMTP")
+        Events("sign", "authed", "conversation", "message")
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
+        Function("address") { ->
+            if (client != null) {
+                client!!.address
+            } else {
+                "No Client."
+            }
+        }
+        // Generate a random wallet and set the client to that
+        Function("createRandom") { ->
+            val privateKey = PrivateKeyBuilder()
+            val client2 = Client().create(account = privateKey)
+            client = client2
+            client!!.address
+        }
     }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(XMTPView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: XMTPView, prop: String ->
-        println(prop)
-      }
-    }
-  }
 }
+
