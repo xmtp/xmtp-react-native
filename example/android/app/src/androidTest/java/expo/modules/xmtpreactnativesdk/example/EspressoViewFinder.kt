0f2e03d9d1e69834a969f345863cc9daee1885a0
Elise Alix <elise@xmtp.com>
2023-06-01 11:44:37 -0400

    run tests

--- example/android/app/src/androidTest/java/expo/modules/xmtpreactnativesdk/example/EspressoViewFinder.kt
+++ example/android/app/src/androidTest/java/expo/modules/xmtpreactnativesdk/example/EspressoViewFinder.kt
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeoutException
 
 object EspressoViewFinder {
     private const val CHECK_INTERVAL = 50L
-    private const val TIMEOUT_MS = 15 * 1000L
+    private const val TIMEOUT_MS = 20 * 1000L
 
     /**
      * Waits for the view referenced in [viewMatcher] to become visible, with a timeout of [timeOut]. If it
