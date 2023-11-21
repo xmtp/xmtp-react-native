8a3ea4a7e69e731329b79f879910c6558c9ca7a9
kele-leanes <ezequiel.leanes@distillery.com>
2023-11-21 09:57:23 -0300

    add conversations documentation

--- src/lib/Conversations.ts
+++ src/lib/Conversations.ts
@@ -11,6 +11,11 @@ export default class Conversations {
     this.client = client;
   }
 
+  /**
+   * This method returns a list of all conversations that the client is a member of.
+   * 
+   * @returns {Promise<Conversation[]>} A Promise that resolves to an array of Conversation objects.
+   */
   async list(): Promise<Conversation[]> {
     const result = await XMTPModule.listConversations(this.client.address);
 
@@ -29,7 +34,15 @@ export default class Conversations {
     this.known[conversation.topic] = true;
     return conversation;
   }
-
+/**
+ * Creates a new conversation.
+ * 
+ * This method creates a new conversation with the specified peer address and context.
+ * 
+ * @param {string} peerAddress - The address of the peer to create a conversation with. 
+ * @param {ConversationContext} context - Optional context to associate with the conversation. 
+ * @returns {Promise<Conversation>} A Promise that resolves to a Conversation object.
+ */
   async newConversation(
     peerAddress: string,
     context?: ConversationContext,
@@ -41,7 +54,16 @@ export default class Conversations {
     );
   }
 
-  async stream(callback: (conversation: Conversation) => Promise<void>) {
+  /**
+   * Sets up a real-time stream to listen for new conversations being started.
+   * 
+   * This method subscribes to conversations in real-time and listens for incoming conversation events.
+   * When a new conversation is detected, the provided callback function is invoked with the details of the conversation.
+   * @param {Function} callback - A callback function that will be invoked with the new Conversation when a conversation is started.
+   * @returns {Promise<void>} A Promise that resolves when the stream is set up.
+   * @warning This stream will continue infinitely. To end the stream, you can call {@linkcode Conversations.cancelStream | cancelStream()}.
+   */
+  async stream(callback: (conversation: Conversation) => Promise<void>): Promise<void> {
     XMTPModule.subscribeToConversations(this.client.address);
     XMTPModule.emitter.addListener(
       "conversation",
@@ -65,9 +87,16 @@ export default class Conversations {
     );
   }
 
+/**
+ * Listen for new messages in all conversations.
+ * 
+ * This method subscribes to all conversations in real-time and listens for incoming and outgoing messages.
+ * @param {Function} callback - A callback function that will be invoked when a message is sent or received.
+ * @returns {Promise<void>} A Promise that resolves when the stream is set up.
+ */
   async streamAllMessages(
     callback: (message: DecodedMessage) => Promise<void>,
-  ) {
+  ): Promise<void> {
     XMTPModule.subscribeToAllMessages(this.client.address);
     XMTPModule.emitter.addListener(
       "message",
@@ -91,10 +120,16 @@ export default class Conversations {
     );
   }
 
+  /**
+   * Cancels the stream for new conversations.
+   */
   cancelStream() {
     XMTPModule.unsubscribeFromConversations(this.client.address);
   }
 
+  /**
+   * Cancels the stream for new messages in all conversations.
+   */
   cancelStreamAllMessages() {
     XMTPModule.unsubscribeFromAllMessages(this.client.address);
   }
