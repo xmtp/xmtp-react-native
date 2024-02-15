0e0f235d4d1b2e7d6a438483065a47ac1bce1602
Naomi Plasterer <naomi@xmtp.com>
2024-02-15 15:48:08 -0800

    register the codec on iOS

--- ios/Wrappers/DecodedMessageWrapper.swift
+++ ios/Wrappers/DecodedMessageWrapper.swift
@@ -41,8 +41,7 @@ struct ContentJson {
 		ReplyCodec(),
 		RemoteAttachmentCodec(),
 		ReadReceiptCodec(),
-		// TODO:
-		// CompositeCodec(),
+		GroupMembershipChangedCodec(),
 	]
 
 	static func initCodecs(client: Client) {
