16ab323bfd2b1e0379c3d0100fa572e8e68b61e4
Naomi Plasterer <naomi@xmtp.com>
2024-04-02 08:15:44 -0700

    fix up linter

--- android/src/main/java/expo/modules/xmtpreactnativesdk/wrappers/DecodedMessageWrapper.kt
+++ android/src/main/java/expo/modules/xmtpreactnativesdk/wrappers/DecodedMessageWrapper.kt
@@ -1,13 +1,11 @@
 package expo.modules.xmtpreactnativesdk.wrappers
 
-import android.util.Log
 import com.google.gson.GsonBuilder
 import org.xmtp.android.library.Client
 import org.xmtp.android.library.codecs.ContentCodec
 import org.xmtp.android.library.codecs.ContentTypeReadReceipt
 import org.xmtp.android.library.codecs.ContentTypeText
 import org.xmtp.android.library.codecs.ReadReceipt
-import org.xmtp.android.library.codecs.decoded
 import org.xmtp.android.library.codecs.description
 import org.xmtp.android.library.messages.DecryptedMessage
 
@@ -27,10 +25,12 @@ class DecodedMessageWrapper {
                     val content = codec.decode(model.encodedContent) as ReadReceipt
                     (codec as ContentCodec<ReadReceipt>).fallback(content).toString()
                 }
-                ContentTypeText  -> {
+
+                ContentTypeText -> {
                     val content = codec.decode(model.encodedContent) as String
                     (codec as ContentCodec<String>).fallback(content).toString()
                 }
+
                 else -> model.encodedContent.fallback
             }
             return mapOf(
