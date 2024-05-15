6c5ba4f6735cce27aa414cf80a0e43a4929c8a2e
Naomi Plasterer <naomi@xmtp.com>
2024-05-15 15:30:04 -0700

    update a test to see if it can be created without a key

--- example/src/tests/groupTests.ts
+++ example/src/tests/groupTests.ts
@@ -26,15 +26,10 @@ function test(name: string, perform: () => Promise<boolean>) {
 
 test('can make a MLS V3 client', async () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const keyBytes = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
   const client = await Client.createRandom({
     env: 'local',
     appVersion: 'Testing/0.0.0',
     enableAlphaMls: true,
-    dbEncryptionKey: keyBytes,
   })
 
   return true
