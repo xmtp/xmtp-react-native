2ce182a769a2bdaed4e56db954833cd8ca7f74b5
Naomi Plasterer <naomi@xmtp.com>
2024-06-04 00:12:52 -0700

    fix: write a test for it

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -184,7 +184,7 @@ export class Client<
       options.env,
       options.appVersion,
       Boolean(options.enableAlphaMls),
-      options.dbEncryptionKey
+      options.dbEncryptionKey,
       options.dbDirectory
     )
 
