41404dbb086d613149246f56313887e21ce71f0c
Naomi Plasterer <naomi@xmtp.com>
2024-09-25 12:29:27 -0600

    get on the latest libxmtp

--- ios/Wrappers/ConversationContainerWrapper.swift
+++ ios/Wrappers/ConversationContainerWrapper.swift
@@ -10,17 +10,17 @@ import XMTP
 
 // Wrapper around XMTP.ConversationContainer to allow passing these objects back into react native.
 struct ConversationContainerWrapper {
-	static func encodeToObj(_ conversation: XMTP.Conversation, client: XMTP.Client) throws -> [String: Any] {
+	static func encodeToObj(_ conversation: XMTP.Conversation, client: XMTP.Client) async throws -> [String: Any] {
 		switch conversation {
 		case .group(let group):
-			return try GroupWrapper.encodeToObj(group, client: client)
+			return try await GroupWrapper.encodeToObj(group, client: client)
 		default:
 			return try ConversationWrapper.encodeToObj(conversation, client: client)
 		}
 	}
 	
-	static func encode(_ conversation: XMTP.Conversation, client: XMTP.Client) throws -> String {
-		let obj = try encodeToObj(conversation, client: client)
+	static func encode(_ conversation: XMTP.Conversation, client: XMTP.Client) async throws -> String {
+		let obj = try await encodeToObj(conversation, client: client)
 		let data = try JSONSerialization.data(withJSONObject: obj)
 		guard let result = String(data: data, encoding: .utf8) else {
 			throw WrapperError.encodeError("could not encode conversation")
