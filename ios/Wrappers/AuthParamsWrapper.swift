c881f711d760a08d19a2753a9baad792f11d71fa
Naomi Plasterer <naomi@xmtp.com>
2024-11-22 13:26:09 -0800

    fix: do the iOS side

--- ios/Wrappers/AuthParamsWrapper.swift
+++ ios/Wrappers/AuthParamsWrapper.swift
@@ -13,48 +13,77 @@ struct AuthParamsWrapper {
 	let appVersion: String?
 	let dbDirectory: String?
 	let historySyncUrl: String?
-	let walletType: WalletType
-	let chainId: Int64?
-	let blockNumber: Int64?
-	
-	init(environment: String, appVersion: String?, dbDirectory: String?, historySyncUrl: String?, walletType: WalletType, chainId: Int64?, blockNumber: Int64?) {
+
+	init(
+		environment: String, appVersion: String?, dbDirectory: String?,
+		historySyncUrl: String?
+	) {
 		self.environment = environment
 		self.appVersion = appVersion
 		self.dbDirectory = dbDirectory
 		self.historySyncUrl = historySyncUrl
-		self.walletType = walletType
-		self.chainId = chainId
-		self.blockNumber = blockNumber
 	}
 
 	static func authParamsFromJson(_ authParams: String) -> AuthParamsWrapper {
 		guard let data = authParams.data(using: .utf8),
-			  let jsonOptions = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
-			return AuthParamsWrapper(environment: "dev", appVersion: nil, dbDirectory: nil, historySyncUrl: nil, walletType: WalletType.EOA, chainId: nil, blockNumber: nil)
+			let jsonOptions = try? JSONSerialization.jsonObject(
+				with: data, options: []) as? [String: Any]
+		else {
+			return AuthParamsWrapper(
+				environment: "dev", appVersion: nil, dbDirectory: nil,
+				historySyncUrl: nil)
 		}
 
 		let environment = jsonOptions["environment"] as? String ?? "dev"
 		let appVersion = jsonOptions["appVersion"] as? String
 		let dbDirectory = jsonOptions["dbDirectory"] as? String
 		let historySyncUrl = jsonOptions["historySyncUrl"] as? String
-		let walletTypeString = jsonOptions["walletType"] as? String ?? "EOA"
-		let chainId = jsonOptions["chainId"] as? Int64
-		let blockNumber = jsonOptions["blockNumber"] as? Int64
-		
-		let walletType = { switch walletTypeString {
-			case "SCW":
-				return  WalletType.SCW
-			default:
-				return WalletType.EOA
-			}
-		}()
-
 
 		return AuthParamsWrapper(
 			environment: environment,
 			appVersion: appVersion,
 			dbDirectory: dbDirectory,
-			historySyncUrl: historySyncUrl,
+			historySyncUrl: historySyncUrl
+		)
+	}
+}
+
+struct WalletParamsWrapper {
+	let walletType: WalletType
+	let chainId: Int64?
+	let blockNumber: Int64?
+
+	init(walletType: WalletType, chainId: Int64?, blockNumber: Int64?) {
+		self.walletType = walletType
+		self.chainId = chainId
+		self.blockNumber = blockNumber
+	}
+
+	static func walletParamsFromJson(_ walletParams: String)
+		-> WalletParamsWrapper
+	{
+		guard let data = walletParams.data(using: .utf8),
+			let jsonOptions = try? JSONSerialization.jsonObject(
+				with: data, options: []) as? [String: Any]
+		else {
+			return WalletParamsWrapper(
+				walletType: WalletType.EOA, chainId: nil, blockNumber: nil)
+		}
+
+		let walletTypeString = jsonOptions["walletType"] as? String ?? "EOA"
+		let chainId = jsonOptions["chainId"] as? Int64
+		let blockNumber = jsonOptions["blockNumber"] as? Int64
+
+		let walletType = {
+			switch walletTypeString {
+			case "SCW":
+				return WalletType.SCW
+			default:
+				return WalletType.EOA
+			}
+		}()
+
+		return WalletParamsWrapper(
 			walletType: walletType,
 			chainId: chainId,
 			blockNumber: blockNumber
