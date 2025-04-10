706d018839055254f236635c828ba74b75611504
cameronvoell <cameronvoell@users.noreply.github.com>
2025-04-10 14:41:08 -0700

    removed accidental duplicated tests

--- example/src/tests/clientTests.ts
+++ example/src/tests/clientTests.ts
@@ -43,445 +43,6 @@ test('can make a client', async () => {
   return true
 })
 
-test('static can get log files', async () => {
-  Client.deactivatePersistentLibXMTPLogWriter()
-  Client.clearXMTPLogs()
-  const originalLogFilePaths = Client.getXMTPLogFilePaths()
-  assert(originalLogFilePaths.length === 0, 'should have log files')
-  // await new Promise(resolve => setTimeout(resolve, 3000))
-  await Client.activatePersistentLibXMTPLogWriter(
-    LogLevel.DEBUG,
-    LogRotation.MINUTELY,
-    10
-  )
-  // await new Promise(resolve => setTimeout(resolve, 3000))
-  const [alix, bo] = await createClients(2)
-
-  const addressMap = await Client.canMessage('local', [
-    alix.publicIdentity,
-    new PublicIdentity(
-      '0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67',
-      'ETHEREUM'
-    ),
-    bo.publicIdentity,
-  ])
-
-  assert(
-    addressMap[
-      '0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67'.toLocaleLowerCase()
-    ] === false,
-    `should not be able to message 0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67`
-  )
-
-  assert(
-    addressMap[alix.publicIdentity.identifier] === true,
-    `should be able to message ${alix.publicIdentity.identifier}`
-  )
-
-  assert(
-    addressMap[bo.publicIdentity.identifier] === true,
-    `should be able to message ${bo.publicIdentity.identifier}`
-  )
-
-  const logFilePaths = Client.getXMTPLogFilePaths()
-  assert(logFilePaths.length > 0, 'should have log files')
-  // await new Promise(resolve => setTimeout(resolve, 3000))
-
-  // Read the first log file and check its contents
-  if (logFilePaths.length > 0) {
-    console.log('num log files', logFilePaths.length)
-    console.log('logFilePaths', logFilePaths[0])
-    Client.deactivatePersistentLibXMTPLogWriter()
-
-    const logContent = await Client.readXMTPLogFile(logFilePaths[0])
-    console.log('logContent length:', logContent.length)
-    console.log('logContent sample:', logContent.substring(0, 200))
-    console.log('logContent', logContent)
-    assert(
-      logContent.includes(alix.inboxId),
-      'Log file should contain the inboxId: ' + alix.inboxId
-    )
-  }
-  Client.deactivatePersistentLibXMTPLogWriter()
-  Client.clearXMTPLogs()
-  return true
-})
-
-test('can revoke all other installations', async () => {
-  const keyBytes = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
-  const alixWallet = Wallet.createRandom()
-
-  // create a v3 client
-  const alix = await Client.create(adaptEthersWalletToSigner(alixWallet), {
-    env: 'local',
-    dbEncryptionKey: keyBytes,
-  })
-
-  await alix.dropLocalDatabaseConnection()
-
-  await alix.deleteLocalDatabase()
-
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-
-  const alix2 = await Client.create(adaptEthersWalletToSigner(alixWallet), {
-    env: 'local',
-    dbEncryptionKey: keyBytes,
-  })
-
-  await alix2.dropLocalDatabaseConnection()
-
-  await alix2.deleteLocalDatabase()
-
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-
-  const alix3 = await Client.create(adaptEthersWalletToSigner(alixWallet), {
-    env: 'local',
-    dbEncryptionKey: keyBytes,
-  })
-
-  const inboxState2 = await alix3.inboxState(true)
-  assert(
-    inboxState2.installations.length === 3,
-    `installations length should be 3 but was ${inboxState2.installations.length}`
-  )
-
-  await alix3.revokeAllOtherInstallations(adaptEthersWalletToSigner(alixWallet))
-
-  const inboxState3 = await alix3.inboxState(true)
-  assert(
-    inboxState3.installations.length === 1,
-    `installations length should be 1 but was ${inboxState3.installations.length}`
-  )
-
-  assert(
-    inboxState3.installations[0].createdAt !== undefined,
-    `installations createdAt should not be undefined`
-  )
-  return true
-})
-
-test('calls preAuthenticateToInboxCallback when supplied', async () => {
-  let isCallbackCalled = 0
-  let isPreAuthCalled = false
-  const preAuthenticateToInboxCallback = () => {
-    isCallbackCalled++
-    isPreAuthCalled = true
-  }
-
-  const keyBytes = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
-
-  await Client.createRandom({
-    env: 'local',
-    preAuthenticateToInboxCallback,
-    dbEncryptionKey: keyBytes,
-  })
-
-  assert(
-    isCallbackCalled === 1,
-    `callback should be called 1 times but was ${isCallbackCalled}`
-  )
-
-  if (!isPreAuthCalled) {
-    throw new Error('preAuthenticateToInboxCallback not called')
-  }
-
-  return true
-})
-
-test('can delete a local database', async () => {
-  let [client, anotherClient] = await createClients(2)
-
-  await client.conversations.newGroup([anotherClient.inboxId])
-  await client.conversations.sync()
-  assert(
-    (await client.conversations.listGroups()).length === 1,
-    `should have a group size of 1 but was ${
-      (await client.conversations.listGroups()).length
-    }`
-  )
-
-  assert(
-    client.dbPath !== '',
-    `client dbPath should be set but was ${client.dbPath}`
-  )
-  await client.dropLocalDatabaseConnection()
-  await client.deleteLocalDatabase()
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-
-  client = await Client.createRandom({
-    env: 'local',
-    dbEncryptionKey: new Uint8Array([
-      233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-      166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135,
-      145,
-    ]),
-  })
-  await client.conversations.sync()
-  assert(
-    (await client.conversations.listGroups()).length === 0,
-    `should have a group size of 0 but was ${
-      (await client.conversations.listGroups()).length
-    }`
-  )
-
-  return true
-})
-
-test('can make a client with encryption key and database directory', async () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const dbDirPath = `${RNFS.DocumentDirectoryPath}/xmtp_db`
-  const directoryExists = await RNFS.exists(dbDirPath)
-  if (!directoryExists) {
-    await RNFS.mkdir(dbDirPath)
-  }
-  const key = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
-
-  const client = await Client.createRandom({
-    env: 'local',
-    dbEncryptionKey: key,
-    dbDirectory: dbDirPath,
-  })
-
-  const anotherClient = await Client.createRandom({
-    env: 'local',
-    dbEncryptionKey: key,
-  })
-
-  await client.conversations.newGroup([anotherClient.inboxId])
-  assert(
-    (await client.conversations.listGroups()).length === 1,
-    `should have a group size of 1 but was ${
-      (await client.conversations.listGroups()).length
-    }`
-  )
-
-  const clientFromBundle = await Client.build(client.publicIdentity, {
-    env: 'local',
-    dbEncryptionKey: key,
-    dbDirectory: dbDirPath,
-  })
-
-  assert(
-    clientFromBundle.inboxId === client.inboxId,
-    `clients dont match ${client.inboxId} and ${clientFromBundle.inboxId}`
-  )
-
-  assert(
-    (await clientFromBundle.conversations.listGroups()).length === 1,
-    `should have a group size of 1 but was ${
-      (await clientFromBundle.conversations.listGroups()).length
-    }`
-  )
-  return true
-})
-
-test('can get a inboxId from an address', async () => {
-  const [alix, bo] = await createClients(2)
-
-  const boInboxId = await alix.findInboxIdFromIdentity(bo.publicIdentity)
-  assert(boInboxId === bo.inboxId, `${boInboxId} should match ${bo.inboxId}`)
-  return true
-})
-
-test('production client creation does not error', async () => {
-  const key = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
-
-  try {
-    await Client.createRandom({
-      env: 'production',
-      dbEncryptionKey: key,
-    })
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    throw error
-  }
-  return true
-})
-
-test('can find others inbox states', async () => {
-  const [alix, bo, caro] = await createClients(3)
-
-  const states = await alix.inboxStates(true, [bo.inboxId, caro.inboxId])
-  assert(
-    states[0].recoveryIdentity.identifier.toLowerCase ===
-      bo.publicIdentity.identifier.toLowerCase,
-    `identities dont match ${states[0].recoveryIdentity} and ${bo.publicIdentity.identifier}`
-  )
-  assert(
-    states[1].identities[0].identifier.toLowerCase ===
-      caro.publicIdentity.identifier.toLowerCase,
-    `clients dont match ${states[1].identities[0]} and ${caro.publicIdentity.identifier}`
-  )
-
-  return true
-})
-
-test('can verify signatures', async () => {
-  const [alix, bo] = await createClients(2)
-  const signature = await alix.signWithInstallationKey('a message')
-
-  assert(
-    (await alix.verifySignature('a message', signature)) === true,
-    `message should verify`
-  )
-
-  assert(
-    (await alix.verifySignature('bad string', signature)) === false,
-    `message should not verify for bad string`
-  )
-
-  assert(
-    (await bo.verifySignature('a message', signature)) === false,
-    `message should not verify for bo`
-  )
-
-  return true
-})
-
-test('test add account with existing InboxIds', async () => {
-  const [alixClient] = await createClients(1)
-
-  const keyBytes = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
-
-  const boWallet = Wallet.createRandom()
-
-  const boClient = await Client.create(adaptEthersWalletToSigner(boWallet), {
-    env: 'local',
-    dbEncryptionKey: keyBytes,
-  })
-
-  let errorThrown = false
-  try {
-    await alixClient.addAccount(adaptEthersWalletToSigner(boWallet))
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    errorThrown = true
-  }
-
-  if (!errorThrown) {
-    throw new Error('Expected addAccount to throw an error but it did not')
-  }
-
-  // Ensure that both clients have different inbox IDs
-  assert(
-    alixClient.inboxId !== boClient.inboxId,
-    'Inbox ids should not be equal'
-  )
-
-  // Forcefully add the boClient account to alixClient
-  await alixClient.addAccount(adaptEthersWalletToSigner(boWallet), true)
-
-  // Retrieve the inbox state and check the number of associated identities
-  const state = await alixClient.inboxState(true)
-  await assertEqual(state.identities.length, 2, 'Length should be 2')
-
-  // Validate that the inbox ID from the address matches alixClient's inbox ID
-  const inboxId = await alixClient.findInboxIdFromIdentity(
-    boClient.publicIdentity
-  )
-  await assertEqual(inboxId, alixClient.inboxId, 'InboxIds should be equal')
-
-  return true
-})
-
-test('can add and remove accounts', async () => {
-  const keyBytes = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
-
-  const alixWallet = Wallet.createRandom()
-  const alixWallet2 = Wallet.createRandom()
-  const alixWallet3 = Wallet.createRandom()
-
-  const alix = await Client.create(adaptEthersWalletToSigner(alixWallet), {
-    env: 'local',
-    dbEncryptionKey: keyBytes,
-  })
-
-  await alix.addAccount(adaptEthersWalletToSigner(alixWallet2))
-  await alix.addAccount(adaptEthersWalletToSigner(alixWallet3))
-
-  const inboxState = await alix.inboxState(true)
-  assert(
-    inboxState.identities.length === 3,
-    `identities length should be 3 but was ${inboxState.identities.length}`
-  )
-  assert(
-    inboxState.installations.length === 1,
-    `identities length should be 1 but was ${inboxState.installations.length}`
-  )
-  assert(
-    inboxState.recoveryIdentity.identifier ===
-      alix.publicIdentity.identifier.toLowerCase(),
-    `recovery address should be ${alix.publicIdentity.identifier} but was ${inboxState.recoveryIdentity}`
-  )
-
-  await alix.removeAccount(
-    adaptEthersWalletToSigner(alixWallet),
-    new PublicIdentity(await alixWallet3.getAddress(), 'ETHEREUM')
-  )
-  const inboxState2 = await alix.inboxState(true)
-  assert(
-    inboxState2.identities.length === 2,
-    `identities length should be 2 but was ${inboxState.identities.length}`
-  )
-
-  return true
-})
-
-test('errors if dbEncryptionKey is lost', async () => {
-  const keyBytes = new Uint8Array([
-    233, 120, 198, 96, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64,
-    166, 83, 208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 74, 252, 135, 145,
-  ])
-  const badKeyBytes = new Uint8Array([
-    0, 0, 0, 0, 154, 65, 132, 17, 132, 96, 250, 40, 103, 35, 125, 64, 166, 83,
-    208, 224, 254, 44, 205, 227, 175, 49, 234, 129, 0, 0, 0, 0,
-  ])
-  const alixWallet = Wallet.createRandom()
-
-  const alix = await Client.create(adaptEthersWalletToSigner(alixWallet), {
-    env: 'local',
-    dbEncryptionKey: keyBytes,
-  })
-
-  let errorThrown = false
-
-  try {
-    await Client.build(alix.publicIdentity, {
-      env: 'local',
-      dbEncryptionKey: badKeyBytes,
-    })
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    errorThrown = true
-  }
-
-  if (!errorThrown) {
-    throw new Error(
-      'Expected build to throw an error with a bad encryption key but it did not'
-    )
-  } else {
-    return false
-  }
-})
-
 test('static can message', async () => {
   const [alix, bo] = await createClients(2)
 
@@ -532,6 +93,67 @@ test('static inboxStates for inboxIds', async () => {
     `inbox state should be ${bo.publicIdentity.identifier} but was ${inboxStates[1].recoveryIdentity.identifier}`
   )
 
+  return true
+})
+
+test('static can get log files', async () => {
+  Client.deactivatePersistentLibXMTPLogWriter()
+  Client.clearXMTPLogs()
+  const originalLogFilePaths = Client.getXMTPLogFilePaths()
+  assert(originalLogFilePaths.length === 0, 'should have log files')
+  await Client.activatePersistentLibXMTPLogWriter(
+    LogLevel.DEBUG,
+    LogRotation.MINUTELY,
+    10
+  )
+  const [alix, bo] = await createClients(2)
+
+  const addressMap = await Client.canMessage('local', [
+    alix.publicIdentity,
+    new PublicIdentity(
+      '0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67',
+      'ETHEREUM'
+    ),
+    bo.publicIdentity,
+  ])
+
+  assert(
+    addressMap[
+      '0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67'.toLocaleLowerCase()
+    ] === false,
+    `should not be able to message 0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67`
+  )
+
+  assert(
+    addressMap[alix.publicIdentity.identifier] === true,
+    `should be able to message ${alix.publicIdentity.identifier}`
+  )
+
+  assert(
+    addressMap[bo.publicIdentity.identifier] === true,
+    `should be able to message ${bo.publicIdentity.identifier}`
+  )
+
+  const logFilePaths = Client.getXMTPLogFilePaths()
+  assert(logFilePaths.length > 0, 'should have log files')
+
+  // Read the first log file and check its contents
+  if (logFilePaths.length > 0) {
+    console.log('num log files', logFilePaths.length)
+    console.log('logFilePaths', logFilePaths[0])
+    Client.deactivatePersistentLibXMTPLogWriter()
+
+    const logContent = await Client.readXMTPLogFile(logFilePaths[0])
+    console.log('logContent length:', logContent.length)
+    console.log('logContent sample:', logContent.substring(0, 200))
+    console.log('logContent', logContent)
+    assert(
+      logContent.includes(alix.inboxId),
+      'Log file should contain the inboxId: ' + alix.inboxId
+    )
+  }
+  Client.deactivatePersistentLibXMTPLogWriter()
+  Client.clearXMTPLogs()
   return true
 })
 
