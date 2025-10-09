c2541aa8fe5f29062b92ea065f3536c48921e78f
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2025-10-09 14:04:02 -0700

    Add creationSignatureKind

--- android/src/main/java/expo/modules/xmtpreactnativesdk/wrappers/InboxStateWrapper.kt
+++ android/src/main/java/expo/modules/xmtpreactnativesdk/wrappers/InboxStateWrapper.kt
@@ -3,23 +3,39 @@ package expo.modules.xmtpreactnativesdk.wrappers
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import org.xmtp.android.library.libxmtp.InboxState
+import org.xmtp.android.library.libxmtp.SignatureKind
 
 class InboxStateWrapper {
     companion object {
         val gson: Gson = GsonBuilder().create()
+
+        private fun signatureKindToString(kind: SignatureKind?): String {
+            return when (kind) {
+                SignatureKind.ERC191 -> "ERC191"
+                SignatureKind.ERC1271 -> "ERC1271"
+                SignatureKind.INSTALLATION_KEY -> "INSTALLATION_KEY"
+                SignatureKind.LEGACY_DELEGATED -> "LEGACY_DELEGATED"
+                SignatureKind.P256 -> "P256"
+                null -> ""
+            }
+        }
         private fun encodeToObj(inboxState: InboxState): Map<String, Any> {
             return mapOf(
-                "inboxId" to inboxState.inboxId,
-                "identities" to inboxState.identities.map { PublicIdentityWrapper.encode(it) },
-                "installations" to inboxState.installations.map {
-                    gson.toJson(
-                        mapOf(
-                            "id" to it.installationId,
-                            "createdAt" to it.createdAt?.time
-                        )
-                    )
-                },
-                "recoveryIdentity" to PublicIdentityWrapper.encode(inboxState.recoveryPublicIdentity)
+                    "inboxId" to inboxState.inboxId,
+                    "identities" to inboxState.identities.map { PublicIdentityWrapper.encode(it) },
+                    "installations" to
+                            inboxState.installations.map {
+                                gson.toJson(
+                                        mapOf(
+                                                "id" to it.installationId,
+                                                "createdAt" to it.createdAt?.time
+                                        )
+                                )
+                            },
+                    "recoveryIdentity" to
+                            PublicIdentityWrapper.encode(inboxState.recoveryPublicIdentity),
+                    "creationSignatureKind" to
+                            signatureKindToString(inboxState.creationSignatureKind)
             )
         }
 
